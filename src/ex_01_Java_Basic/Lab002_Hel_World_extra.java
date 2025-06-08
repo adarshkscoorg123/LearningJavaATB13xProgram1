3bd1ba1be1818e68060a2349bebfab279eb82346
S <adarsh-k.s@capgemini.com>
2025-06-08 13:03:30 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab002_Hel_World_extra.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Hel_World_extra {
+
+    public static void main(String[] args) {
+        System.out.println("The sum of 2 numbers is " +(2+2));
+    }
+}
