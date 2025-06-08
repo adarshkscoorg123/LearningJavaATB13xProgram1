0192196d8a6bb2548c8c4f802ea024187621c75b
adarshks <adarsh_coorg123@yahoo.com>
2025-06-08 14:47:00 +0530

    New lab002 added

--- src/ex_01_Java_Basic/Lab002_Hel_World_extra.java
+++ src/ex_01_Java_Basic/Lab002_Hel_World_extra.java
@@ -3,6 +3,6 @@ package ex_01_Java_Basic;
 public class Lab002_Hel_World_extra {
 
     public static void main(String[] args) {
-        System.out.println("The sum of 2 numbers is " +(2+2));
+        System.out.println("The sum of 3 numbers is " +(2+3));
     }
 }
