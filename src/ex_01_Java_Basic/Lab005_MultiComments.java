78f2809454ab7e813983a7788225d91c2aff2676
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-06-09 20:30:08 +0530

    New Lab005 added

---
+++ src/ex_01_Java_Basic/Lab005_MultiComments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab005_MultiComments {
+
+    public static void main(String[] args) {
+        /*System.out.println("Hi");
+        System.out.println("bye");
+        System.out.println("drink");
+        System.out.println("chai");*/
+    }
+}
