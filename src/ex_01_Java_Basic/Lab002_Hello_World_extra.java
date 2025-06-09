9e967137de5c58439aef788f1ad66a19620c8079
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-06-09 21:17:18 +0530

    New Lab006 added

--- src/ex_01_Java_Basic/Lab002_Hello_World_extra.java
+++ src/ex_01_Java_Basic/Lab002_Hello_World_extra.java
@@ -3,6 +3,7 @@ package ex_01_Java_Basic;
 public class Lab002_Hello_World_extra {
 
     public static void main(String[] args) {
+
         System.out.println("The sum of 3 numbers is " +(2+3));
     }
 }
---
+++ src/ex_01_Java_Basic/Lab006_Keywords_Identifiers.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Keywords_Identifiers {
+
+    public static void main(String[] args) {
+        System.out.println("How many keywords do you see here, how many identifiers do we see");
+    }
+}
