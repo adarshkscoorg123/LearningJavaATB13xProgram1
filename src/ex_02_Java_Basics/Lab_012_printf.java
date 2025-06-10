0b708862626396f5f5c8545d3d91809893feb34c
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-06-10 20:40:44 +0530

    New lab_012 added

---
+++ src/ex_02_Java_Basics/Lab_012_printf.java
@@ -0,0 +1,21 @@
+package ex_02_Java_Basics;
+
+public class Lab_012_printf {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("print command print without the new line");
+        System.out.print("Hello");
+        System.out.println("World");
+
+        System.out.println(a);
+        System.out.printf("Your variable name is %d",a);
+
+        int b= 20;
+        int c= a*b;
+        System.out.println("----");
+        System.out.printf("%d + %d",a,b);
+        System.out.println(" ");
+        System.out.println(c);
+    }
+}
