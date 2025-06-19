ffdc6b337d40a915e63c6c24ada8f6152f82ef9c
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-06-19 08:14:10 +0530

    19th June code

---
+++ src/ex_07_Java_Test/Lab_42_VarDecl_Initial.java
@@ -0,0 +1,20 @@
+package ex_07_Java_Test;
+
+public class Lab_42_VarDecl_Initial {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        double b = 128899999;
+
+        boolean b1 = true;
+
+        char c = 'z';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(c);
+    }
+}
---
+++ src/ex_07_Java_Test/Lab_43_ArithmeticAssignment.java
@@ -0,0 +1,23 @@
+package ex_07_Java_Test;
+
+public class Lab_43_ArithmeticAssignment {
+
+    public static void main(String[] args) {
+
+        int a = 40;
+        int b = 20;
+
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiply = a * b;
+        int division = a / b;
+        int modulus =  a % b;
+
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiply);
+        System.out.println(division);
+        System.out.println(modulus);
+
+    }
+}
