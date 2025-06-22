35b3543aceb22fb984a4e90b117532ffe900fa05
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-06-22 12:27:46 +0530

    13th June code- remaining code

---
+++ src/ex_09_Java_IF/Lab_055_Interview.java
@@ -0,0 +1,15 @@
+package ex_09_Java_IF;
+
+public class Lab_055_Interview {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        if(a == 4){
+            System.out.println("Haha");
+        }
+        else{
+            System.out.println("HoHo");
+        }
+    }
+}
