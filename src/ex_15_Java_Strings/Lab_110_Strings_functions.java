9f46674a28a9df77a3558419f48ebb1b886d4436
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-06-29 14:25:36 +0530

    23rd June code- part1

---
+++ src/ex_15_Java_Strings/Lab_110_Strings_functions.java
@@ -0,0 +1,15 @@
+package ex_15_Java_Strings;
+
+public class Lab_110_Strings_functions {
+
+    public static void main(String[] args) {
+
+        String name = "Sonal";
+        System.out.println(name.length()); // 5
+        System.out.println(name.charAt(2)); // n
+        System.out.println(name.charAt(3)); // a
+        System.out.println(name.charAt(1)); // o
+        System.out.println(name.charAt(10)); // String out of bound exception
+
+    }
+}
