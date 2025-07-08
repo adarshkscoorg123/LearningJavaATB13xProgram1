4a4970df54e069af11dd9b2a317b2a313a96d297
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-07-08 20:10:01 +0530

    2nd July code

---
+++ src/ex_21_Java_MultiLevelInheritance/Son_171_multiple.java
@@ -0,0 +1,9 @@
+package ex_21_Java_MultiLevelInheritance;
+
+public class Son_171_multiple extends Father1_169_multiple, Father2_170_multiple{
+
+    // You can't have multiple Inheritance
+    // You can't take from multiple people-classes
+
+
+}
