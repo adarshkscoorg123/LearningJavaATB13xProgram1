1517cd096a8a7a7f2adcfcab6bb0966f3cf1fee4
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-12-05 11:44:04 +0530

    5th Dec-2025

--- src/ex_31_Java_Static/Lab_234_Static.java
+++ src/ex_31_Java_Static/Lab_234_Static.java
@@ -5,10 +5,12 @@ public class Lab_234_Static {
     public static void main(String[] args) {
         Pramod p1 = new Pramod();
         Pramod p2 = new Pramod();
+        Pramod p3 = new Pramod();
         System.out.println(Pramod.a); // static variable can be accessed through class name
         Pramod.drink(); // static method can be accessed through class name
         p1.a = 18;
         p2.a = 20;
+        p3.a = 30;
         System.out.println(Pramod.a); // same value since it is static
         System.out.println(p1.a); // same value since it is static
         System.out.println(p2.a);// same value since it is static
@@ -17,9 +19,8 @@ public class Lab_234_Static {
         p2.b = 45;
         System.out.println(p1.b); // different since it is not static
         System.out.println(p2.b); // different since it is not static
-
+        System.out.println(p3.b);
     }
-
 }
 
 class Pramod{
@@ -29,9 +30,10 @@ class Pramod{
 
     public static void main(String[] args) {
         Pramod p1 = new Pramod();
+        drink();
+
+
     }
-
-
 
     static {
         // This is a static block
@@ -42,19 +44,26 @@ class Pramod{
      static void drink(){
 
         System.out.println("Drinking");
+        //dream();
+    }
+
+    static void dream(){
+
+        System.out.println("Dreaming");
+        //drink();
     }
 
     void eat(){
 
         System.out.println("Eating");
-       drink();
-       sleep();
+        drink();
+        sleep();
         System.out.println();
     }
 
     void sleep(){
 
         System.out.println("sleep");
-        eat();
+        //eat();
     }
 }
